594a7963917f63019afb05766e4ce5c6c1435ba6
Jillian Weimer <jilliannweimer@gmail.com>
2020-07-22 12:42:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,8 @@ def oxford_comma(array)
     return "#{array[0]} and #{array[1]}"
   elsif array.length == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length < 3
+    array[-1].insert(0, "and")
   end
   array.join(",")
 end
