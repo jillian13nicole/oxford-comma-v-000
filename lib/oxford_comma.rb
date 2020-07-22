0a20e64b1753fc70a2189172e4380d8af7f443ad
Jillian Weimer <jilliannweimer@gmail.com>
2020-07-22 12:37:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 2
     return "#{array[0]} and #{array[1]}"
-  else array == 3
+  else array.length == 3
     return "#{array[0]}, #{array[1]}, and #{array[2]}"
   end
   array.join(",")
